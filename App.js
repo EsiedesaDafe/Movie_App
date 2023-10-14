78ac840b380b62d10fcb811a76f872d91dc6c28a
EsiedesaDafe <esiedesadafe@gmail.com>
2023-10-14 12:59:13 +0100

    update

--- App.js
+++ App.js
@@ -1,37 +1,79 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from "@react-navigation/stack";
-import Onboardingscreen from './SRC/onboardScreens/onboardingscreen';
-import Custom from './SRC/BottomTabs/Custom';
-import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react-native'
-import { Amplify } from 'aws-amplify';
-import awsmobile from './SRC/aws-exports';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionic from 'react-native-vector-icons/Ionicons';
 
-Amplify.configure(awsmobile);
+import { FirstScreenNavigator} from './SRC/CustomNav/CustomNav';
+import { SecondScreenNavigator} from './SRC/CustomNav/CustomNav';
+import { ThirdScreenNavigator} from './SRC/CustomNav/CustomNav';
+import { FourthScreenNavigator} from './SRC/CustomNav/CustomNav';
 
-const Stack = createStackNavigator();
-
+const Tab = createBottomTabNavigator();
 
 const App = () => {
-
-  return (
+  return(
     <>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-        headerShown: false,
-        headerTintColor: 'white',
-        headerStyle: { backgroundColor: 'tomato' },
-      }}>
-          <Stack.Screen name="Onboardingscreen" component={Onboardingscreen}></Stack.Screen>
-          <Stack.Screen name="Custom" component={Custom}></Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+      <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            tabBarActiveTintColor: '#e91e63',
+            headerShown: false,
+            tabBarInactiveTintColor: '#fff',
+            tabBarStyle: { backgroundColor: '#000'}
+          }}
+        >
+          <Tab.Screen
+            name="Screen 1"
+            component={FirstScreenNavigator} 
+            options={{
+              headerShown: false,
+              tabBarLabel: 'Home',
+              tabBarIcon: ({ color, size }) => (
+                <Ionic name="ios-home" color={color} size={size} />
+              ),
+            }}  
+          />
+          <Tab.Screen
+            name="Screen 2"
+            component={SecondScreenNavigator} 
+            options={{
+              headerShown: false,
+              tabBarLabel: 'Download',
+              tabBarIcon: ({ color, size }) => (
+                <Ionic name="ios-cloud-download" color={color} size={size} />
+              ),
+              tabBarBadge: 3,
+            }}  
+          />
+          <Tab.Screen
+            name="Screen 3"
+            component={ThirdScreenNavigator} 
+            options={{
+              headerShown: false,
+              tabBarLabel: 'Notification',
+              tabBarIcon: ({ color, size }) => (
+                <Ionic name="ios-notifications" color={color} size={size} />
+              ),
+            }} 
+          />
+          <Tab.Screen
+            name="Screen 4"
+            component={FourthScreenNavigator} 
+            options={{
+              headerShown: false,
+              tabBarLabel: 'Settings',
+              tabBarIcon: ({ color, size }) => (
+                <Ionic name="ios-settings" color={color} size={size} />
+              ),
+            }} 
+          />
+        </Tab.Navigator>
+        </NavigationContainer></>
   );
 };
 
 
 export default App;
-
 
---
+++ SRC/CustomNav/SignIn.js
@@ -0,0 +1,123 @@
+import React from "react";
+import { View, Text, ImageBackground,TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
+const SignIn = () => {
+
+    const [text, onChangeText] = React.useState('Useless Text');
+
+    const [fontsLoaded] = useFonts({
+        'Poppins-SemiBold': require('../../assets/fonts/Poppins/Poppins-SemiBold.ttf'),
+        'Poppins-Medium': require('../../assets/fonts/Poppins/Poppins-Medium.ttf'),
+        'Poppins-Regular': require('../../assets/fonts/Poppins/Poppins-Regular.ttf'),
+        'Poppins-Light': require('../../assets/fonts/Poppins/Poppins-Light.ttf'),
+    });
+
+      
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+        }
+      }, [fontsLoaded]);
+    
+      if (!fontsLoaded) {
+        return null;
+      }
+
+    return(
+        <ImageBackground source={require('../../assets/IMAGES/Z.png')} onLayout={onLayoutRootView}>
+
+            <View style={style.FirstTextView}>
+                <Text style={style.FirstText}>SignUp</Text>
+            </View>
+
+            <View style={style.FirstInputView}>
+                <Text style={style.NameText}>Enter Name</Text>
+                <TextInput 
+                value={text}
+                placeholder="name"
+                onChangeText={onChangeText}
+                style={style.FirstInput}/>
+            </View>
+
+            <View style={style.SecondInputView}>
+                <Text style={style.NameText}>Enter Password</Text>
+                <TextInput 
+                value={text}
+                placeholder="Enter Password"
+                onChangeText={onChangeText}
+                style={style.FirstInput}/>
+            </View>
+
+            <View style={style.LoginTextStyle}>
+                <TouchableOpacity>
+                    <Text style={style.LoginViewStyle}>
+                        Sign In
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
+        </ImageBackground>
+    )
+};
+const style = StyleSheet.create({
+    container: {
+
+    },
+    FirstText: {
+        color: 'red',
+        fontSize: 33,
+        fontWeight: '500',
+    },
+    FirstTextView: {
+        marginTop: 20,
+        paddingHorizontal: 25,
+    },
+    NameText: {
+        fontSize: 18,
+        color: 'red',
+        fontWeight: '300',
+        lineHeight: 16,
+        fontStyle: 'normal',
+    },
+    FirstInput: {
+        display: 'flex',
+        borderRadius: 4,
+        backgroundColor: '#272A2F',
+        height: 48,
+        width: 370,
+        alignSelf: 'center',
+        marginTop: 5,
+    },
+    FirstInputView: {
+        marginHorizontal: 25,
+        marginTop: 50,
+    },
+    SecondInputView: {
+        marginHorizontal: 25,
+        marginTop: 15,
+    },
+    LoginTextStyle: {
+        fontSize: 20,
+        fontWeight: '400',
+        color: 'white',
+        lineHeight: 'normal',
+    },
+    LoginViewStyle: {
+        marginHorizontal: 10,
+        paddingHorizontal: 15,
+        marginTop: 15,
+        width: 388,
+        height: 52,
+        justifyContent: 'center',
+        display: 'flex',
+        color: 'white',
+        backgroundColor: 'red',
+    },
+});
+
+export default SignIn;
